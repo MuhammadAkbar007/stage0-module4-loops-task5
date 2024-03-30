e5432bc7b6a0997e56f607d5aa2870b386c266cf
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-03-30 23:13:44 +0500

    whitespaces removed 🗑

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -4,7 +4,7 @@ public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < length; j++) {
-                System.out.print(" 8 ");
+                System.out.print("8");
             }
             System.out.println();
         }
