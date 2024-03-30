3e1c9bb934e894523743c793dbf29e06c3d242df
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-03-30 23:06:04 +0500

    Bismillah 🥇

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        // Print upper half of the hourglass
+        for (int i = height; i >= 1; i--) {
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
 
+        // Print lower half of the hourglass
+        for (int i = 2; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
